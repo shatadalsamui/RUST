2e5cd5e38cfd8e558bc0faabc17e086eacb0d641
shatadalsamui <shatadalsamuimain@gmail.com>
2025-11-25 17:24:47 +0530

    .

--- RUST0-1/src/mutability.rs
+++ RUST0-1/src/mutability.rs
@@ -3,7 +3,11 @@ fn main() {
     //x = 2 ; this line will show error as by deafult variables ion rust are immutable
     println!(" x : {}", x);
 
-    let mut y: i32 = 2;// add the mut keyowrds make it mutable meaning it can be changed or updated ; 
+    let mut y: i32 = 2; // add the mut keyowrds make it mutable meaning it can be changed or updated ; 
     y = 3;
-    println!("y : {}", y)
+    println!("y : {}", y);
+
+    let mut z = String::from("hi there"); // mutability in strings 
+    z.push_str("Asd");
+    println!("{}", z);
 }
