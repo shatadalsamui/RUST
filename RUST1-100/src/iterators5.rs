3c59d399c0b9da5baa80188657ca518c860b9f5e
shatadalsamui <shatadalsamuimain@gmail.com>
2025-12-04 20:59:15 +0530

    .

--- RUST1-100/src/iterators5.rs
+++ RUST1-100/src/iterators5.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let nums = vec![1, 2, 3];
+    let nums = vec![1, 2, 3, 4, 5, 6];
 
-    for value in nums.into_iter() {
-        println!("{}", value);
-    }
+    let odds: Vec<_> = nums.into_iter().filter(|x| x % 2 != 0).collect();
+
+    println!("{:?}", odds); // [1, 3, 5]
 }
